115eb571f74f7f916aceecd97c59024d36d0077b
Rubyrubylai <r844312@gmail.com>
2026-03-14 20:14:47 +0100

    chore: support test output in terminal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,3 +26,11 @@ java {
         languageVersion = JavaLanguageVersion.of(21)
     }
 }
+
+tasks.test {
+    testLogging {
+        events("passed", "skipped", "failed")
+        showStandardStreams = true
+        exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+    }
+}
